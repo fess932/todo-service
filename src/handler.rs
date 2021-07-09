261f277e58bb3ba748b0d073ab4d4127bb1ddc6c
Ivan Cheremisin <fess932@gmail.com>
2021-07-10 01:49:12 +0300

    switch to rocket

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,41 @@
-use hyper::{Body, Request, Response};
-use std::convert::Infallible;
+use rocket::data::{Data, ToByteUnit};
+use rocket::tokio;
 
-pub async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+#[post("/hello", data = "<data>")]
+pub async fn hello_world(data: Data<'_>) -> std::io::Result<()> {
+    data.open(512.kibibytes())
+        .stream_to(tokio::io::stdout())
+        .await?;
+    Ok(())
+
+    // let mut response = Response::new(Body::empty());
+
+    // match (req.method(), req.uri().path()) {
+    //     (&Method::GET, "/") => *response.body_mut() = Body::from("Try posting data to /echo"),
+    //     (&Method::POST, "/echo") => {
+    //         *response.body_mut() = req.into_body();
+    //     }
+    //     (&Method::POST, "/echo/uppercase") => {
+    //         let mapping = req.into_body().map_ok(|chunk| {
+    //             chunk
+    //                 .iter()
+    //                 .map(|byte| byte.to_ascii_uppercase())
+    //                 .collect::<Vec<u8>>()
+    //         });
+
+    //         *response.body_mut() = Body::wrap_stream(mapping);
+    //     }
+    //     (&Method::POST, "/echo/reverse") => {
+    //         let full_body = hyper::body::to_bytes(req.into_body()).await?;
+
+    //         let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
+
+    //         *response.body_mut() = reversed.into();
+    //     }
+    //     _ => {
+    //         *response.status_mut() = StatusCode::NOT_FOUND;
+    //     }
+    // };
+
+    // Ok(response)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Server;
+use rocket::tokio::time::{sleep, Duration};
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-use std::convert::Infallible;
-use std::net::SocketAddr;
 use std::str::FromStr;
 
 mod handler;
@@ -10,36 +7,25 @@ mod handler;
 const CREATE_TABLE: &str =
     "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)";
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Wanted for {} seconds", seconds)
+}
+
+#[launch]
+async fn rocket() -> _ {
     prepare().await.expect("wrong prepared db");
-    // sqlx::query(
-    //     "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)",
-    // )
-    // .fetch(&pool)
-    // .await?;
 
-    // let row: (i64,) = sqlx::query_as("SELECT $1")
-    //     .bind(150_i64)
-    //     .fetch_one(&pool)
-    //     .await?;
-
-    // assert_eq!(row.0, 150);
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
-
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handler::hello_world)) });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    let graceful = server.with_graceful_shutdown(shutdown_signal());
-
-    if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
-    }
-
-    Ok(())
+    rocket::build().mount("/", routes![index, delay, handler::hello_world])
 }
 
 async fn prepare() -> Result<(), sqlx::Error> {
@@ -56,11 +42,4 @@ async fn prepare() -> Result<(), sqlx::Error> {
         .expect("create table users failed");
 
     Ok(())
-}
-
-async fn shutdown_signal() {
-    tokio::signal::ctrl_c()
-        .await
-        .expect("failed to install CTRL+C signal hanlder");
-    println!("gracefull shutdown by CTRL+C");
 }
