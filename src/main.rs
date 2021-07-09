11c5d6210469ce40ce393faa5f4ca45284c1a88e
Ivan Cheremisin <fess932@gmail.com>
2021-07-09 21:53:38 +0300

    sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::Server;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use sqlx::sqlite::SqlitePoolOptions;
+use std::str::FromStr;
+
+mod handler;
+
+const CREATE_TABLE: &str =
+    "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)";
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = SqlitePoolOptions::new().connect("sqlite:db.sqlite3").await?;
+    prepare().await.expect("wrong prepared db");
+    // sqlx::query(
+    //     "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)",
+    // )
+    // .fetch(&pool)
+    // .await?;
 
     // let row: (i64,) = sqlx::query_as("SELECT $1")
     //     .bind(150_i64)
-    //     .fetch_one(&pool).await?;
+    //     .fetch_one(&pool)
+    //     .await?;
 
     // assert_eq!(row.0, 150);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
 
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handler::hello_world)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
@@ -29,8 +42,20 @@ async fn main() -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn prepare() -> Result<(), sqlx::Error> {
+    let conn = SqliteConnectOptions::from_str("sqlite:db.sqlite3")?.create_if_missing(true);
+
+    let pool = SqlitePoolOptions::new()
+        .connect_with(conn)
+        .await
+        .expect("pool failed");
+
+    sqlx::query(CREATE_TABLE)
+        .execute(&pool)
+        .await
+        .expect("create table users failed");
+
+    Ok(())
 }
 
 async fn shutdown_signal() {
